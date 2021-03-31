0456b2b4d78d066410ba63b7a9bac9eff2130de2
tlpackus <tlpackus@gmail.com>
2021-03-31 15:54:57 -0700

    update test import to Age object

---
+++ __tests__/spaceage.test.js
@@ -0,0 +1,11 @@
+import { TestScheduler } from 'jest';
+import Age from './../src/spaceage.js';
+// describe('Age', () => {
+//   let ageObject;
+//   beforeEach(() => {
+//     ageObject = new Age(23, 100);
+//   });
+//   test('should correctly create an Age object' () => {
+//     expect(ageObject.age).toEqual
+//   })
+// })
