f5e58f9dc8808c7110454aafbde59d9d4624a54d
tlpackus <tlpackus@gmail.com>
2021-03-31 16:07:19 -0700

    add ageObject instance and good fail for Age constructor

--- __tests__/spaceage.test.js
+++ __tests__/spaceage.test.js
@@ -1,11 +1,12 @@
-import { TestScheduler } from 'jest';
+// import { TestScheduler } from 'jest';
 import Age from './../src/spaceage.js';
-// describe('Age', () => {
-//   let ageObject;
-//   beforeEach(() => {
-//     ageObject = new Age(23, 100);
-//   });
-//   test('should correctly create an Age object' () => {
-//     expect(ageObject.age).toEqual
-//   })
-// })
+describe('Age', () => {
+  let ageObject;
+  beforeEach(() => {
+    ageObject = new Age(23, 100);
+  });
+  test('should correctly create an Age object', () => {
+    expect(ageObject.age).toEqual(23);
+    expect(ageObject.lifeExpectancy).toEqual(100);
+  });
+});
