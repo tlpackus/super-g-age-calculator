0762414ed0a3d2f7ec24e02288449c28b7669a05
tlpackus <tlpackus@gmail.com>
2021-04-01 13:33:23 -0700

    add passing test for .ageCalc() method

--- src/spaceage.js
+++ src/spaceage.js
@@ -4,6 +4,17 @@ export default class Age {
     this.lifeExpectancy = lifeExpectancy;
   }
   ageCalc(planet) {
-
+    if (planet==="mercury") {
+      return Math.floor(this.age/.24);
+    }
+    else if (planet==="venus") {
+      return Math.floor(this.age/.62);
+    }
+    else if (planet==="mars") {
+      return Math.floor(this.age/1.88);
+    }
+    else if (planet==="jupiter") {
+      return Math.floor(this.age/11.86);
+    }
   }
 }
